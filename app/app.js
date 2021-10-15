a213efc338db11f4636e49fad694227fa52d00b8
facutorraca <ftorraca@fi.uba.ar>
2021-10-15 19:17:25 -0300

    integration with grafana

--- app/app.js
+++ app/app.js
@@ -1,32 +1,33 @@
 const express = require("express");
+const http = require('http')
 
 const app = express();
 const SERVER_PORT = 3000;
-
-app.get("/", (req, res) => {
-  res.status(200).send("ping");
-});
 
 app.listen(SERVER_PORT, () => {
   console.log(`Listening at port ${SERVER_PORT}.`);
 });
 
+app.get("/", (req, res) => {
+  res.status(200).send("ping");
+});
+
 app.get('/proxy-sincronico', (req, res) => {
-  request.get('http://box:9090/').on('response', (response) => {
+  http.get('http://2c21-tp-1_bbox_1:9090/').on('response', (response) => {
     console.log("proxy sincronico" + response.statusCode)
     res.status(response.statusCode).send("proxy sincronico")
   })
 });
 
 app.get('/proxy-asincronico', (req, res) => {
-  request.get('http://box:9091/').on('response', (response) => {
+  http.get('http://2c21-tp-1_bbox_1:9091/').on('response', (response) => {
     console.log("proxy asincronico" + response.statusCode)
     res.status(response.statusCode).send("proxy asincronico")
   })
 });
 
-app.get('/intensivo', (req, res) => {
-  let start = new Date();
-  while ((start.getSeconds() - (new Date()).getSeconds()) < 3) { }
-  res.status(200).send('intensivo')
-})
+ app.get('/intensivo', (req, res) => {
+   let start = new Date();
+   while ((start.getSeconds() - (new Date()).getSeconds()) < 3) { }
+   res.status(200).send('intensivo')
+ })
