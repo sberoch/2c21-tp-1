681ae93be576d20ddf193073887af4bd58b4a078
facutorraca <ftorraca@fi.uba.ar>
2021-10-17 11:27:36 -0300

    tests

--- app/app.js
+++ app/app.js
@@ -1,33 +1,40 @@
 const express = require("express");
-const http = require('http')
+const http = require('http');
 
 const app = express();
 const SERVER_PORT = 3000;
+
+const log = (endpoint, statusCode) => {
+  console.log(`${endpoint} | ${statusCode} | ${new Date().toISOString()}`);
+}
 
 app.listen(SERVER_PORT, () => {
   console.log(`Listening at port ${SERVER_PORT}.`);
 });
 
 app.get("/ping", (req, res) => {
-  res.status(200).send("ping");
+  log('PING', 200);
+  res.status(200).send("PING" + '\n');
 });
 
-app.get('/proxy-sincronico', (req, res) => {
+app.get('/bbox-1', (req, res) => {
   http.get('http://2c21-tp-1_bbox_1:9090/').on('response', (response) => {
-    console.log("proxy sincronico" + response.statusCode)
-    res.status(response.statusCode).send("proxy sincronico")
+    log('BBOX-1', response.statusCode);
+    res.status(response.statusCode).send('BBOX-1' + '\n');
   })
 });
 
-app.get('/proxy-asincronico', (req, res) => {
+app.get('/bbox-2', (req, res) => {
   http.get('http://2c21-tp-1_bbox_1:9091/').on('response', (response) => {
-    console.log("proxy asincronico" + response.statusCode)
-    res.status(response.statusCode).send("proxy asincronico")
+    log('BBOX-2', response.statusCode);
+    res.status(response.statusCode).send('BBOX-2' + '\n');
   })
 });
 
- app.get('/intensivo', (req, res) => {
+ app.get('/intensive', (req, res) => {
    let start = new Date();
    while ( ( (new Date().getTime() - start.getTime() ) / 1000 ) < 3);
-   res.status(200).send('intensivo')
+   log('INTENSIVE', 200);
+   res.status(200).send('INTENSIVE' + '\n')
  })
+
